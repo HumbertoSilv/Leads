6f57b51d42ebd59adecedc9d943cc9241bc72e15
Humberto <matheushumberto520@gmail.com>
2021-09-28 20:55:05 -0400

    att: renamed URL

--- app/routes/lead_blueprint.py
+++ app/routes/lead_blueprint.py
@@ -3,10 +3,10 @@ from app.controllers.leads_controller import (
     update_lead, get_leads, register_lead, delete_lead
 )
 
-bp = Blueprint("lead_bp", __name__, url_prefix="/lead")
+bp = Blueprint("lead_bp", __name__, url_prefix="/api")
 
 
-bp.post("")(register_lead)
-bp.get("")(get_leads)
-bp.patch("")(update_lead)
-bp.delete("")(delete_lead)
+bp.post("lead")(register_lead)
+bp.get("lead")(get_leads)
+bp.patch("lead")(update_lead)
+bp.delete("lead")(delete_lead)
